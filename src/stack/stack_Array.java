a3713ab6f25292ce1c775b0267f4bee203f8ced8
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-07-29 18:21:00 +0530

    committed

---
+++ src/stack/stack_Array.java
@@ -0,0 +1,77 @@
+package stack;
+
+import java.util.Scanner;
+
+class stack {
+    int n = 10; // max size
+    int top = -1;
+    int a[] = new int[n]; // array
+
+    void push(Scanner sc) {
+        if (top == (n - 1)) {
+            System.out.println("Overflow");
+        } else {
+            System.out.println("Enter data");
+            int i = sc.nextInt();
+            top = top + 1;
+            a[top] = i;
+            System.out.println("Item inserted");
+        }
+    }
+
+    void pop() {
+        if (top == -1) {
+            System.out.println("Underflow");
+        } else {
+            top = top - 1;
+            System.out.println("Item deleted");
+        }
+    }
+
+    void display() {
+        System.out.println("Display items here:");
+        for (int j = top; j >= 0; j--) {
+            System.out.print(a[j] + " ");
+        }
+        System.out.println(); // Move to the next line after displaying all items
+    }
+}
+
+public class stack_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        stack s = new stack();
+        int d;
+        int l;
+
+        do {
+            System.out.println("Press 1 to push");
+            System.out.println("Press 2 to pop");
+            System.out.println("Press 3 to display");
+            System.out.println("Enter your choice");
+            d = sc.nextInt();
+            switch (d) {
+                case 1: {
+                    s.push(sc);
+                    break;
+                }
+                case 2: {
+                    s.pop();
+                    break;
+                }
+                case 3: {
+                    s.display();
+                    break;
+                }
+            }
+
+            System.out.println("Enter 0 to go back to main menu");
+            System.out.println("Enter any key to exit");
+
+            l = sc.nextInt();
+        }
+        while (l==0);
+        System.out.println("exit");
+        }
+    }
+
---
+++ src/stack/stack_LinkedList.java
@@ -0,0 +1,106 @@
+package stack;
+
+import linkedlist.Single_LL;
+
+import java.util.Scanner;
+
+class stack1{
+
+    static class Node //static because we don't need to create object of this class
+    {    //inner class
+        int data;
+        Node next;
+
+        Node(int data) //constrcutor
+
+        {
+            this.data = data; //to initialise the initial value of variables in the class
+            this.next = null;
+
+        }
+    }
+        Node top=null;//initially top is null
+
+
+    //push
+    void push(Scanner sc)
+    {
+        System.out.println("Enter data");
+        int data =sc.nextInt();
+        Node new_node=new Node(data);
+        if(top==null)
+        {
+            top=new_node;
+        }
+        else
+        {
+            new_node.next=top;
+            top=new_node;
+        }
+    }
+
+    void pop(){
+        if(top==null)
+        {
+            System.out.println("stack is empty");
+        }
+        else
+        {
+            top=top.next;
+        }
+        }
+
+        void display(){
+        Node temp=top;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+        }
+    }
+
+
+
+
+
+
+
+public class stack_LinkedList {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        stack1 s = new stack1();
+        int d;
+        int l;
+
+        do {
+            System.out.println("Press 1 to push");
+            System.out.println("Press 2 to pop");
+            System.out.println("Press 3 to display");
+            System.out.println("Enter your choice");
+            d = sc.nextInt();
+            switch (d) {
+                case 1: {
+                    s.push(sc);
+                    break;
+                }
+                case 2: {
+                    s.pop();
+                    break;
+                }
+                case 3: {
+                    s.display();
+                    break;
+                }
+            }
+
+            System.out.println("Enter 0 to go back to main menu");
+            System.out.println("Enter any key to exit");
+
+            l = sc.nextInt();
+        }
+        while (l==0);
+        System.out.println("exit");
+    }
+}
+
